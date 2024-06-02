26c48abccaee4ef2f4cc7381d6ffe674f26c8d60
chrisrekfild <chrisrekfild@mail.ru>
2024-06-03 00:17:43 +0500

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Fitness
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        readonly MainWindow mainWindow;
+
+        // через систему внедрения зависимостей получаем объект главного окна
+        public App(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            mainWindow.Show();  // отображаем главное окно на экране
+            base.OnStartup(e);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            // создаем хост приложения
+            var host = Host.CreateDefaultBuilder()
+                // внедряем сервисы
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<App>();
+                    services.AddSingleton<MainWindow>();
+                })
+                .Build();
+            var app = host.Services.GetService<App>();
+            // запускаем приложения
+            app?.Run();
+        }
+    }
+}
