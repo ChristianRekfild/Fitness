d077d358c6ef7a683b7e8de54f120582489db562
chrisrekfild <chrisrekfild@mail.ru>
2024-06-03 23:24:26 +0500

    Добавлено чуть автодокументации к прошлому коммиту

--- Views/Base/ViewModelBase.cs
+++ Views/Base/ViewModelBase.cs
@@ -21,10 +21,10 @@ namespace Fitness.Views.Base
         /// Устанавливает значение поля, при этом разрешая проблему кольцевых изменений
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="field"></param>
-        /// <param name="value"></param>
-        /// <param name="propertyName"></param>
-        /// <returns></returns>
+        /// <param name="field">Поле, в которое происходит запись</param>
+        /// <param name="value">Новое значение, которое будет записано в поле (при успехе, см. ниже)</param>
+        /// <param name="propertyName">имя свойства. Можно не указывать. P.S. да, я ленив =)</param>
+        /// <returns>True, если значение успешно изменено. False, если данные в поле уже совпадают с тем, что мы хотим туда записать</returns>
         protected virtual bool SetValue<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
         {
             if (Equals(field, value)) return false;
