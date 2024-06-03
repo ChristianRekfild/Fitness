bcd2fb28de3cf24d49810ca6c208b25dcd3b7419
chrisrekfild <chrisrekfild@mail.ru>
2024-06-03 23:22:06 +0500

    Подготовлен базовый класс ViewModelBase для последующей работы в других ViewModel. Сейчас сделано, чтобы потом можно было лениться

---
+++ Views/Base/ViewModelBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.Views.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnProperyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Устанавливает значение поля, при этом разрешая проблему кольцевых изменений
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="value"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        protected virtual bool SetValue<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnProperyChanged(propertyName);
+
+            return true;
+        }
+    }
+}
